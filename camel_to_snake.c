a3c6224a3c695e54baae05bd2a6fa321cc83a260
Hasan Kemal ECE <109526257+ecehasankemal@users.noreply.github.com>
2022-10-14 17:06:31 +0300

    Add files via upload

---
+++ camel_to_snake.c
@@ -0,0 +1,66 @@
+#include <unistd.h>
+#include <stdlib.h>
+
+int	ft_strlen(char *str)
+{
+	int	count;
+
+	count = 0;
+	while (str[count])
+		count++;
+	return (count);
+}
+
+void	ft_putstr(char *str)
+{
+	int	iter;
+
+	iter = 0;
+	while (str[iter] != '\0')
+	{
+		write (1, &str[iter], 1);
+		iter++;
+	}
+}
+
+void	camel_to_snake(char *str)
+{
+	int	iter;
+	int	index;
+	int	value;
+	char	*string;
+
+	iter = 0;
+	value = 0;
+	index = 0;
+	while (str[iter] != '\0')
+	{
+		if (str[iter] >= 65 && str[iter] <= 90)
+			value++;
+		iter++;
+	}
+	iter = 0;
+	string = (char *)malloc(sizeof(char) * (value + ft_strlen(str)));
+	while (str[iter] != '\0')
+	{
+		if(str[iter] >= 65 && str[iter] <= 90)
+		{
+			string[index] = '_';
+			string[++index] = str[iter];
+		}
+		else
+			string[index] = str[iter];
+		iter++;
+		index++;
+	}
+	ft_putstr(string);
+}
+
+int	main(int ac, char **av)
+{
+	if (ac == 2)
+	{
+		camel_to_snake(av[1]);
+	}
+	write (1, "\n", 1);
+}
