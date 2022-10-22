2c43e3ddc95d11702823eea619679646698a3403
Hasan Kemal ECE <109526257+ecehasankemal@users.noreply.github.com>
2022-10-22 11:34:36 +0300

    Add files via upload

---
+++ paramsum.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb > 9)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else
+		ft_putchar(nb + '0');
+}
+
+int	main(int ac, char **av)
+{
+	ft_putnbr(ac - 1);
+
+	return (0);
+}
